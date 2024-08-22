a159bf879aba72554ca570b61d1cd13c71411428
rakeshkgang <rakeshkgang8057@gmail.com>
2024-08-22 16:53:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World hello\n');
+  res.end('Rakesh Gang\n');
 });
 
 server.listen(port, hostname, () => {
